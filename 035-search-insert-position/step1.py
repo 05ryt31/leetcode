30867f2b13d818c221d886433521008a0c90814b
Rytxxx <k.ryuto.5@icloud.com>
2025-11-29 14:33:51 -0800

    035-search-insert-position

---
+++ 035-search-insert-position/step1.py
@@ -0,0 +1,7 @@
+from typing import List
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        mid = nums.len() / 2
+        if target > List[mid]:
+            
