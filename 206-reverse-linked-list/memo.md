816b08b697803bcb81d2620371effae522ffad3e
Rytxxx <k.ryuto.5@icloud.com>
2026-01-06 02:52:35 -0800

    feat: reverse-linked-list

---
+++ 206-reverse-linked-list/memo.md
@@ -0,0 +1,13 @@
+## step1で考えたこと
+Linked Listのvalをstackに積んでいき、最後まで辿り着いたら順に取り出していけばreverseできる
+
+pointerをどう入れ替えるかが鍵？
+
+## step2
+後半のポインタ操作が若干怪しかった。
+
+run_timeは早いが、space efficiency的には微妙. 
+
+Time Comp: O(n)
+
+Space Comp: O(n)
---
+++ 206-reverse-linked-list/step1.py
@@ -0,0 +1,19 @@
+# 動かないコード
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not None:
+            return None
+
+        reverse = []
+        reverse.add(this.val)
+
+        while this.next:
+            reverse.append(this.next)
+
+        return reverse
